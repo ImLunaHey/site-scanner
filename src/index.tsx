8b78b1df2a29d8e8d82f813fa4f6e37ff0c006f9
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-10-14 07:58:33 +1030

    fix: add charset

--- src/index.tsx
+++ src/index.tsx
@@ -176,7 +176,7 @@ const createResponse = async (element: ReactElement) => {
     // Fetch stats
     const queries = await axiom.query(`['site-scanner'] | where eventType == 'query' | count | project count=Count`).then(result => result.matches?.[0].data.count ?? 0).catch(() => 0);
     const scans = await axiom.query(`['site-scanner'] | where eventType == 'result' | count | project count=Count`).then(result => result.matches?.[0].data.count ?? 0).catch(() => 0);
-    return new Response(minify('<!doctype html><html lang="en"><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>' + renderToStaticMarkup(<>
+    return new Response(minify('<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head>' + renderToStaticMarkup(<>
         {element}
         <footer><span title={footerDescription}>Scans: {scans}</span> | Queries: {queries}</footer>
     </>) + '</html>', {
